6bd57c086681cb3603aa71a62617cf83b09576b6
Ashrafuj Jaman Tanbin <ashrafuj.jaman.tanbin1@gmail.com>
2024-02-27 18:06:36 +0600

    some more workdone

--- src/app/modules/User/user.model.ts
+++ src/app/modules/User/user.model.ts
@@ -24,8 +24,6 @@ const userSchema = new Schema<TUser,UserModel>({
     },
     imageLink: {
         type: String,
-        required: true,
-        default:'no image found'
     },
    
     email:{
